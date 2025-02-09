c7ff5bb1aaf4bfb3ae204d54bad1bcc947c5605b
Sajmul <thepioneer126@gmail.com>
2025-02-09 14:44:53 +0600

    more efficient

--- prime.c
+++ prime.c
@@ -1,23 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main () {
+void main()
+{
     int num;
     printf("Enter a number:");
     scanf("%d", &num);
     int isPrime = 1;
 
-    for (int i = 2; i < num; i++)
+    if (num == 0 || num == 1)
     {
-        if (num % i == 0)
-        {
-            isPrime = 0;
-            break;
-        }        
+        printf("%d is not prime number", num);
     }
+    else
+    {
 
-    if(isPrime) {
-        printf("This is a prime number");
-    } else {
-        printf("This is not a prime number.");
-    }    
+        int halfNum = num / 2;
+
+        for (int i = 2; i <= halfNum; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime)
+        {
+            printf("%d is a prime number.", num);
+        }
+        else
+        {
+            printf("%d is not a prime number.", num);
+        }
+    }
 }
