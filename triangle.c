1daa45f92c4975b9bcd043e5b301b6fb781d2223
Sajmul <thepioneer126@gmail.com>
2025-02-09 14:18:01 +0600

    triangle problem solved

--- triangle.c
+++ triangle.c
@@ -6,7 +6,7 @@ void main() {
     printf("Enter three sides of a triangle:");
     scanf("%d %d %d", &side1, &side2, &side3);
 
-    if(side1 + side2 >= side3 || side2 + side3 >= side1 || side1 + side3 >= side2 ) {
+    if(side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2 ) {
         if (side1 == side1 && side2 == side3) 
         {
             printf("This is a Equilateral Triangle");
